6b2a8961257d8d9e4f4e2c8e64f448ad12b53ab1
MaximeO <maxime.oliveira40@gmail.com>
2023-11-16 12:09:29 +0100

    Création de la classe personne

---
+++ Personne.cpp
@@ -0,0 +1,10 @@
+/**
+ * @project Intellight
+ * @file Personne.cpp
+ * @brief Classe personne
+ * @authors OLIVEIRA LOPES Maxime
+ *          NAJI Ines
+ * @date 16/11/2023
+ */
+
+#include "Personne.h"
---
+++ Personne.h
@@ -0,0 +1,13 @@
+/**
+ * @project Intellight
+ * @file Personne.h
+ * @brief Header Classe personne
+ * @authors OLIVEIRA LOPES Maxime
+ *          NAJI Ines
+ * @date 16/11/2023
+ */
+
+#ifndef BECPPOBJETSCONNECTES_PERSONNE_H
+#define BECPPOBJETSCONNECTES_PERSONNE_H
+
+#endif //BECPPOBJETSCONNECTES_PERSONNE_H
