2cd82b3dba5fb0e7cefbba750767d776ff111322
MaximeO <maxime.oliveira40@gmail.com>
2023-11-21 15:12:04 +0100

    Classe personne: attributs perso, attributs de classe, préférences user

--- Personne.cpp
+++ Personne.cpp
@@ -8,3 +8,33 @@
  */
 
 #include "Personne.h"
+
+// Initialisation de la variable de classe
+short Personne::nbPersonnes = 0;
+
+// Constructeurs:
+Personne::Personne() {
+    nbPersonnes++;
+    id = nbPersonnes;
+    username = "NULL";
+    isHere = false;
+
+    // Par défaut, on règle la lumière sur une lumière blanche à 100% d'intensité
+    lightIntensity = 100.0;
+    R = 255;
+    G = 255;
+    B = 255;
+}
+
+Personne::Personne(std::string us_name) {
+    nbPersonnes++;
+    id = nbPersonnes;
+    username = us_name;
+    isHere = false;
+
+    // Par défaut, on règle la lumière sur une lumière blanche à 100% d'intensité
+    lightIntensity = 100.0;
+    R = 255;
+    G = 255;
+    B = 255;
+}
--- Personne.h
+++ Personne.h
@@ -10,4 +10,54 @@
 #ifndef BECPPOBJETSCONNECTES_PERSONNE_H
 #define BECPPOBJETSCONNECTES_PERSONNE_H
 
+#include <iostream>
+#include <string>
+//#include "Piece.h"
+
+// (Idée de classe dérivée: personne prioritaire?)
+
+class Personne {
+private:
+    /* Variables de classe */
+    static short nbPersonnes;
+
+
+    /* Données générales sur l'utilisateur */
+    short id; // Identifiant de l'utilisateur -- remplacer par l'ID du tag NFC ?
+    std::string username; // Nom d'utilisateur
+    bool isHere; // Statut de présence de l'utilisateur dans le domicile
+//    Piece::Piece currentRoom; // Pièce dans laquelle l'utilisateur est présent
+
+
+    /* Données de préférence de l'utilisateur */
+    float lightIntensity;
+    short R, G, B;
+
+public:
+    /* Constructeurs */
+    Personne();
+    Personne(std::string us_name);
+
+
+    /* Méthodes */
+    // Getters
+    short getId() const;
+    std::string getUsername() const;
+    bool getPresence() const;
+//    Piece::Piece getCurrentRoom() const;
+
+
+    // Setters
+    void setUsername(std::string u);
+    void setPresence(bool p);
+//    void setCurrentRoom(Piece::Piece p);
+
+
+    // Autres méthodes
+
+
+    // Destructeurs
+    ~Personne() = default;
+};
+
 #endif //BECPPOBJETSCONNECTES_PERSONNE_H
