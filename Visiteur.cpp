c330bb32aed9368c48462dc94c640799c985759b
MaximeO <maxime.oliveira40@gmail.com>
2023-11-23 10:24:56 +0100

    Ajout des constructeurs et du destructeur (pour le moment par défaut) de Visiteur

--- Visiteur.cpp
+++ Visiteur.cpp
@@ -1,5 +1,22 @@
-//
-// Created by maxim on 23/11/2023.
-//
+/**
+ * @project Intellight
+ * @file Visiteur.cpp
+ * @brief Classe Visiteur
+ * @authors OLIVEIRA LOPES Maxime
+ *          NAJI Ines
+ * @date 16/11/2023
+ */
 
 #include "Visiteur.h"
+
+Visiteur::Visiteur() : Personne() {
+    autorise = false;
+}
+
+Visiteur::Visiteur(std::string us_name) : Personne(us_name) {
+    autorise = false;
+}
+
+Visiteur::Visiteur(std::string us_name, bool a) : Personne(us_name){
+    autorise = a;
+}
