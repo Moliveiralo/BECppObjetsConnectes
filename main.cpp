6bac1e24edb2fffaeca09a82da8c062760c75c59
MaximeO <maxime.oliveira40@gmail.com>
2023-11-24 14:31:32 +0100

    Test de la classe Personne

--- main.cpp
+++ main.cpp
@@ -19,10 +19,14 @@ int main(){
     Personne personne1("Maxime");
     Personne personne2;
 
+    personne2.setUsername("Ines");
+
     personne1.setLightPreferences(100, 214, 137, 56);
     short * tabPref = personne1.getLightPreferences();
+    short * tabPref2 = personne2.getLightPreferences();
 
-    cout << tabPref[0] << " " << tabPref[1] << " " << tabPref[2] << endl;
+    cout << "Les preferences de " << personne1.getUsername() << " sont : " << tabPref[0] << " " << tabPref[1] << " " << tabPref[2] << endl;
+    cout << "Les preferences de " << personne2.getUsername() << " sont : " << tabPref2[0] << " " << tabPref2[1] << " " << tabPref2[2] << endl;
 
     return 0;
 }
