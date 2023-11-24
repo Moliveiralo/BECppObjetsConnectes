fa1f34b1076d049c5f09b7186be24b647836a908
MaximeO <maxime.oliveira40@gmail.com>
2023-11-24 14:18:27 +0100

    test Personne

--- main.cpp
+++ main.cpp
@@ -8,9 +8,21 @@
  */
 
 #include <iostream>
+#include "Appartement.h"
+#include "Personne.h"
+#include "Piece.h"
+#include "Visiteur.h"
 
 using namespace std;
 
 int main(){
+    Personne personne1("Maxime");
+    Personne personne2;
+
+    personne1.setLightPreferences(100, 214, 137, 56);
+    short * tabPref = personne1.getLightPreferences();
+
+    cout << tabPref[0] << " " << tabPref[1] << " " << tabPref[2] << endl;
+
     return 0;
 }
