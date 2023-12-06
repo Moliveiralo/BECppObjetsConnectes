e65bedd353a5ebba0d29ea24ca1b242f25ab6595
MaximeO <maxime.oliveira40@gmail.com>
2023-12-06 18:46:23 +0100

    Ajout et implémentation des méthodes de numpad

--- intellight/Numpad.cpp
+++ intellight/Numpad.cpp
@@ -3,3 +3,41 @@
 //
 
 #include "Numpad.h"
+
+Numpad::Numpad() {
+    data = 0;
+    Rx = 0;
+    Tx = 0;
+    serial = nullptr;
+}
+
+Numpad::Numpad(short tx, short rx, SoftwareSerial& s) {
+    data = 0;
+    Rx = rx;
+    Tx = tx;
+    serial = s;
+}
+
+uint8_t Numpad::getData() {
+    return data;
+}
+
+short Numpad::getRx() {
+    return Rx;
+}
+
+short Numpad::getTx() {
+    return Tx;
+}
+
+void Numpad::setRx(short rx) {
+    Rx = rx;
+}
+
+void Numpad::setTx(short tx) {
+    Tx = tx;
+}
+
+void Numpad::updateData() {
+    data = serial->read();
+}
